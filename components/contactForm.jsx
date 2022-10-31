52ee8b4e39c431eb40ac6d465f41734b3de72ea8
Sufyan <xufyanshahid23@gmail.com>
2022-10-31 17:59:12 +0500

    Change contact form emial id

--- components/contactForm.jsx
+++ components/contactForm.jsx
@@ -59,7 +59,7 @@ const ContactForm = () => {
     // console.log(checkboxHTML);
     const res = await fetch("/api/mail", {
       body: JSON.stringify({
-        email: "accounts@techdots.dev",
+        email: "hina@techdots.dev",
         subject: subject,
         fromName: `${name} - Techdots`,
         message: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
