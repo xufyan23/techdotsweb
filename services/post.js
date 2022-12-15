6129e4f276d04d29dbd154b5bc2637fa1d1c3fd1
Sufyan <xufyanshahid23@gmail.com>
2022-12-15 17:32:50 +0500

    Remove extra spaces

--- services/post.js
+++ services/post.js
@@ -2,26 +2,26 @@ import imageUrlBuilder from "@sanity/image-url";
 
   const getPosts = async (customQuery = undefined) => {
 
-	const query = encodeURIComponent(customQuery || '*[_type == "post"]');
-	const url = `https://p3umg9xf.api.sanity.io/v1/data/query/production?query=${query}`;
-	const result = await fetch(url).then(res => res.json());
+  const query = encodeURIComponent(customQuery || '*[_type == "post"]');
+  const url = `https://p3umg9xf.api.sanity.io/v1/data/query/production?query=${query}`;
+  const result = await fetch(url).then(res => res.json());
 
-	const posts = result.result
-	if (!posts || !posts.length) {
-		return [];
-	}
+  const posts = result.result
+  if (!posts || !posts.length) {
+    return [];
+  }
 
-	const imgBuilder = imageUrlBuilder({
-		projectId: 'p3umg9xf',
-		dataset: 'production'
-	});
+  const imgBuilder = imageUrlBuilder({
+    projectId: 'p3umg9xf',
+    dataset: 'production'
+  });
 
-	return posts.map(post => {
-		return {
-			...post,
-			mainImage: post.mainImage ? imgBuilder.image(post.mainImage).url(): null,
-		}
-	});
+  return posts.map(post => {
+    return {
+      ...post,
+      mainImage: post.mainImage ? imgBuilder.image(post.mainImage).url(): null,
+    }
+  });
 }
 
 export default getPosts;
