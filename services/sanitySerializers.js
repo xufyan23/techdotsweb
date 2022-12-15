ea81520571b9654809ecfb90db6cfd9693a1115a
Sufyan <xufyanshahid23@gmail.com>
2022-12-15 17:58:48 +0500

    Added code block

--- services/sanitySerializers.js
+++ services/sanitySerializers.js
@@ -1,16 +1,9 @@
-import { PreviewCode } from "@sanity/code-input";
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atomOneDark } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 
 const serializers = {
   types: {
     code: (props) => {
-      console.log('sadsad', props, PreviewCode({
-        selection: {
-          language: props.node.language,
-          code: props.node.language
-        }
-      }));
       return<SyntaxHighlighter style={atomOneDark}>
       {props.node.code}
       </SyntaxHighlighter>
